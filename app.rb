e5b32c90443ba2e8c63ac1960af687abc4a06c1f
vedranvinko <vinkovedran@gmail.com>
2022-04-28 13:13:10 +0200

    Example output from app

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require 'fluent-logger'
 require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/' do
     puts 'Hello from fluentd-ruby'
+    Fluent::Logger::FluentLogger.open(nil, :host=>'localhost', :port=>24224)
+    Fluent::Logger.post("fluentd.test.follow", {"from"=>"userA", "to"=>"userB"})
   end
 end
